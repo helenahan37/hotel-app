032ba128a8dc32762f7715b80dd64ae34a1100f4
Helena.Han <hlyhelena37@gmail.com>
2024-02-06 22:44:18 +1100

    feat: CRUD reservations methods

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
 
-  constructor() { }
+  //get all reservations
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  //get reservation by id
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  //add new reservation
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  //delete reservation by id
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex((res) => res.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  //update reservation by id
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(
+      (res) => res.id === updatedReservation.id
+    );
+    this.reservations[index] = updatedReservation;
+  }
 }
